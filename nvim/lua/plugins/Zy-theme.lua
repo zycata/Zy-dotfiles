8ba083935f17eedcde0b66b14365c64884fc6703
Zyc <166348388+zycata@users.noreply.github.com>
2026-04-22 18:10:53 -0700

    using relative paths instead of hardcoedd paths

--- nvim/lua/plugins/Zy-theme.lua
+++ nvim/lua/plugins/Zy-theme.lua
@@ -1,7 +1,8 @@
-
+-- if true then return {} end 
 return {
   {
-    dir = "~/.config/nvim/Zy-theme", -- Path to your local folder
+    -- dir = "~/.config/nvim/Zy-theme", -- Path to your local folder
+    dir = vim.fn.stdpath "config" .. "/" .. "Zy-theme/",
     lazy = false,       
     priority = 1000,    -- Load before everything else
     config = function()
